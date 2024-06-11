70deb753ca289a4034069dea6bb4ebfa4b8c6a0c
healac <smx.m03alba.helena@gmail.com>
2024-06-11 16:09:31 +0200

    añadir mostrar usuarios

--- menu.php
+++ menu.php
@@ -8,5 +8,6 @@
 		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
 		<h3> <b>En construcció!!!!!!!!!!!</b> </h3>
 		<a href="http://zend-dacomo.fjeclot.net/autent/index.php">Torna a la pàgina inicial</a>
+		<a href="http://zend-dacomo.fjeclot.net/autent/index.php">Mostrar dades d'usuari</a>
 	</body>
 </html>

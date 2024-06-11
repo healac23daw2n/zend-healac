c89902069e6be1c7939d19ac730682a897814e79
healac <smx.m03alba.helena@gmail.com>
2024-06-11 16:45:07 +0200

    nuevos archivos, crear , eliminar, atributos

--- menu.php
+++ menu.php
@@ -8,6 +8,10 @@
 		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
 		<h3> <b>En construcció!!!!!!!!!!!</b> </h3>
 		<a href="http://zend-dacomo.fjeclot.net/autent/index.php">Torna a la pàgina inicial</a>
-		<a href="http://zend-dacomo.fjeclot.net/autent/index.php">Mostrar dades d'usuari</a>
+		<a href="http://zend-dacomo.fjeclot.net/autent/mostrar_dades_usuari.php">Mostrar dades d'usuari</a>
+		<a href="http://zend-dacomo.fjeclot.net/autent/eliminar_usuari.php">eliminar usuari</a>
+		<a href="http://zend-dacomo.fjeclot.net/autent/atributos.php">afegir atributs a l'usuari</a>
+		<a href="http://zend-dacomo.fjeclot.net/autent/crear.php">crear usuari</a>
+		
 	</body>
 </html>
